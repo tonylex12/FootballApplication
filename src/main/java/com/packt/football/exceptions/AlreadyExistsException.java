fb6bdfd0fbac887930cda2493adb1212d021f782
Tony HUerto <tonylex12@gmail.com>
2024-10-17 11:13:57 -0500

    Simple Football API

---
+++ src/main/java/com/packt/football/exceptions/AlreadyExistsException.java
@@ -0,0 +1,9 @@
+package com.packt.football.exceptions;
+
+public class AlreadyExistsException extends RuntimeException {
+
+  public AlreadyExistsException(String message) {
+    super(message);
+  }
+
+}
